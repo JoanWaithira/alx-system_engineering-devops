d4dd2f487e82c45c4102a603edf65903f84cd58d
JoanWaithira <joanwaithira.jw@gmail.com>
2024-01-03 00:44:47 -0800

    Task Zero

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A script that returns infrmatin about an employee"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_name = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed_tasks = [h.get("title") for h in todo if h.get("completed")
+                       is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name.get("name"), len(completed_tasks), len(todo)))
+    [print("\t {}".format(i)) for i in completed_tasks]
